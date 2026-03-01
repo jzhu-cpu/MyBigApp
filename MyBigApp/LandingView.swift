d146375071bb23f8cbc7563e9cbdd209d82fffe2
Judy <jzhu@lcs.on.ca>
2026-03-01 13:46:21 -0500

    V and H stack on the bottom

--- MyBigApp/LandingView.swift
+++ MyBigApp/LandingView.swift
@@ -9,11 +9,21 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        
+    
+        
+        
+        
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            VStack {
+                Rectangle ()
+            }
+            
+            HStack {
+               Rectangle()
+                Rectangle()
+            }
+          
         }
         .padding()
     }
---
+++ MyBigApp/WeatherView.swift
@@ -0,0 +1,7 @@
+//
+//  WeatherView.swift
+//  MyBigApp
+//
+//  Created by Judy Z on 2026/3/1.
+//
+

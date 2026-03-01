f9a81611abd27bc66b266525b207b955d3d38164
Judy <jzhu@lcs.on.ca>
2026-03-01 13:10:06 -0500

    Initial Commit

---
+++ MyBigApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyBigApp
+//
+//  Created by Judy Z on 2026/3/1.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MyBigApp/MyBigAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyBigAppApp.swift
+//  MyBigApp
+//
+//  Created by Judy Z on 2026/3/1.
+//
+
+import SwiftUI
+
+@main
+struct MyBigAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

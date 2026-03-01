8bc553ec939031a5c14c5e3a4e14458a35630cf1
Judy <jzhu@lcs.on.ca>
2026-03-01 14:14:31 -0500

    Added Title

--- MyBigApp/LandingView.swift
+++ MyBigApp/LandingView.swift
@@ -9,8 +9,12 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        
+        Text("Smith-Ennismore-Lakefield")
+        .font(.largeTitle)
     
+       
+        
+            
         
         
         

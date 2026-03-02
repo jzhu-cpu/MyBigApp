62be3217cf59155aeff6ea0631b072d142870f0a
Judy <jzhu@lcs.on.ca>
2026-03-02 14:45:36 -0500

    Added Navigation title

---
+++ Views/SwiftUIView.swift
@@ -0,0 +1,26 @@
+//
+//  SwiftUIView.swift
+//  MyBigApp
+//
+//  Created by Judy Z on 2026/3/2.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+
+        NavigationStack {
+            Text("")
+                .navigationTitle("􀎞 Home Page")
+     
+           
+                }
+        }
+    }
+
+            #Preview {
+                SwiftUIView()
+            }
+            
+     

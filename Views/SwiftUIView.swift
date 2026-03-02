9716713330dbc43a2ca5a6f00a7bb6b5d8d97066
Judy <jzhu@lcs.on.ca>
2026-03-02 15:08:12 -0500

    Added horizontal divider

--- Views/SwiftUIView.swift
+++ Views/SwiftUIView.swift
@@ -9,18 +9,52 @@ import SwiftUI
 
 struct SwiftUIView: View {
     var body: some View {
-
+        
         NavigationStack {
             Text("")
                 .navigationTitle("􀎞 Home Page")
-     
-           
-                }
-        }
-    }
-
-            #Preview {
-                SwiftUIView()
-            }
             
-     
+            
+            HStack {
+                Text("ww")
+                    .font(.largeTitle).lineLimit(1)
+                Image(systemName: "globe" )                   .resizable()
+                    .frame(width: 80, height: 80)
+                   
+                Text("rty")
+                    .font(.largeTitle)
+                Image(systemName: "globe")       .resizable()
+                    .frame(width:80,height:80)
+                
+                    .layoutPriority(1) // Truncate last
+            }
+            .padding([.horizontal])
+            Divider()
+            HStack {
+                Text("wwww")
+                    .font(.largeTitle)
+                    .layoutPriority(1) // Truncate last
+                Image(systemName:"globe")
+             
+                    .resizable()
+                    .frame(width: 80, height: 80)
+                Text("fghj")
+                Image(systemName:"globe")     .resizable()
+                    .frame(width:80,height:80)
+                    .font(.largeTitle).lineLimit(1) // Don't let text wrap
+            }
+            .padding(.horizontal)
+        }
+        
+        
+        
+        
+    }
+}
+
+
+#Preview {
+    SwiftUIView()
+}
+
+
